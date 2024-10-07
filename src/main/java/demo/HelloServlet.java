59fa869ef6ea0fbfba533d41a4a5300c8764c8bd
kenduraghav <kenduraghav@gmail.com>
2024-10-07 15:10:17 +0800

    Code formatted

--- src/main/java/demo/HelloServlet.java
+++ src/main/java/demo/HelloServlet.java
@@ -14,31 +14,30 @@ import gg.jte.TemplateEngine;
 import gg.jte.TemplateOutput;
 import gg.jte.output.StringOutput;
 
-
 @WebServlet("/hello-jte")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	TemplateEngine templateEngine;
-	
-    public HelloServlet() {
-        super();
-        templateEngine = TemplateEngine.createPrecompiled(Path.of("jte-classes"), ContentType.Html);
-        //templateEngine = TemplateEngine.createPrecompiled(ContentType.Html);;
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	TemplateEngine templateEngine;
+
+	public HelloServlet() {
+		super();
+		templateEngine = TemplateEngine.createPrecompiled(Path.of("jte-classes"), ContentType.Html);
+		// templateEngine = TemplateEngine.createPrecompiled(ContentType.Html);;
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		TemplateOutput output = new StringOutput();
 		String name = "John Doe";
 		templateEngine.render("index.jte", name, output);
 		System.out.println(output);
-		
+
 		response.setContentType("text/html");
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
 }
